af75b38c3ac238af1b53f88735862229d3d8991e
Bryan Van de Ven <bryanv@continuum.io>
2013-09-05 10:43:53 -0500

    add main.coffee

---
+++ src/coffee/main.js
@@ -0,0 +1,10 @@
+require.config
+  paths:
+    jquery: "vendor/jquery/jquery"
+    jquery-ui: "vendor/jquery-ui-amd/jquery-ui-1.10.0/jqueryui"
+    jquery-mousewheel: "vendor/jquery-mousewheel/jquery.mousewheel"
+    underscore: "vendor/underscore-amd/underscore"
+    backbone: "vendor/backbone-amd/backbone"
+    bootstrap: "vendor/bootstrap/dist/js/bootstrap"
+
+
