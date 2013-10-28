340e6d0493957dc4cdbe8c2957203bae0ca9ace9
Paddy Mullen <paddy@paddymullen.com>
2013-10-28 15:53:14 -0400

    a working asynchronous test

--- tests/tests.js
+++ tests/tests.js
@@ -2,10 +2,18 @@ module("Basic Tests");
 
 
 test("truthy", function() {
-    require(["/js/adder.js"], function(adder_mod){
-            alert(adder_mod.adder(5,5));
-    });
     ok(true, "true is truthy");
     equal(1, true, "1 is truthy");
     notEqual(0, true, "0 is NOT truthy");
+});
+
+asyncTest("an asynchonous test example", function() {
+    expect(1);
+    require(["/js/adder.js"], function(adder_mod){
+        equal(adder_mod.adder(5,5), 10);
+        start();
+    });
+
+
+
 });
