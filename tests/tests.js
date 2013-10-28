65cfbcf09f3e523a50a220145304a66450a5ac32
Paddy Mullen <paddy@paddymullen.com>
2013-10-28 15:58:29 -0400

    a normal synchronous test with require.js

--- tests/tests.js
+++ tests/tests.js
@@ -1,16 +1,12 @@
-module("Basic Tests");
+define(["/js/adder.js"], function(adder){
+    module("Basic Tests");
+    test("truthy", function() {
+        ok(true, "true is truthy");
+        equal(1, true, "1 is truthy");
+        notEqual(0, true, "0 is NOT truthy");
+    });
 
-
-test("truthy", function() {
-    ok(true, "true is truthy");
-    equal(1, true, "1 is truthy");
-    notEqual(0, true, "0 is NOT truthy");
-});
-
-asyncTest("an asynchonous test example", function() {
-    expect(1);
-    require(["/js/adder.js"], function(adder_mod){
-        equal(adder_mod.adder(5,5), 10);
-        start();
+    test("synch normal test example", function() {
+            equal(adder.adder(5,5), 10);
     });
 });
