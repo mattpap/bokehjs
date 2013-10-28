51e7b2bebd5e883b000efb0a0014c80a23cc0282
Paddy Mullen <paddy@paddymullen.com>
2013-10-28 15:41:07 -0400

    a test that runs in a webbrowser that uses require

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,10 @@
 module("Basic Tests");
 
+
 test("truthy", function() {
+    require(["/js/adder.js"], function(adder_mod){
+            alert(adder_mod.adder(5,5));
+    });
     ok(true, "true is truthy");
     equal(1, true, "1 is truthy");
     notEqual(0, true, "0 is NOT truthy");
